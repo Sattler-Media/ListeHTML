3f47aea7ecb06f31c8a0cd667893b421b21f45ae
Adrian Maldonado <adrian.maldonado@sattler.media>
2025-04-09 13:23:26 +0200

    Switch Ordnung commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+/* Search button */
 document.getElementById('searchInput').addEventListener('input', function() {
     const query = this.value.toLowerCase();
     document.querySelectorAll('.list-group-item').forEach(item => {
@@ -5,10 +6,11 @@ document.getElementById('searchInput').addEventListener('input', function() {
         item.style.display = text.includes(query) ? '' : 'none';
     });
 });
-
+/* Dunkel Mode */
 document.getElementById('flexSwitchCheckDefault').addEventListener('click', () => {
     document.body.classList.toggle('dark-mode');
 });
+/* Videos Pause */
 document.querySelectorAll('iframe').forEach(iframe => {
     iframe.onload = () => {
         iframe.contentWindow.postMessage('{"method":"pause"}', '*');
